6b34487f677393b784cd137d6aa75b9c8a2eecac
achsalsa <achsls9206@gmail.com>
2023-06-24 16:04:56 +0200

    modification au niveau des JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,12 @@
 
 const textInputs=document.querySelectorAll("input[type='text']");
+const regintegers=new RegExp(/^\d*$/);
+const regdecimals=new RegExp(/^\d+(\.\d*)?$/);
+
+let tip=parseInt(document.querySelector(".input-group ul li.active input").dataset.value);
+let bill=0;
+let personsNumber=1;
+
 textInputs.forEach((textinput)=>{
     let prevval=textinput.value;
     textinput.addEventListener('beforeinput', function(e){
@@ -7,7 +14,50 @@ textInputs.forEach((textinput)=>{
     });
 
     textinput.addEventListener('input', function(ev){
-        console.log(this.dataset.prevVale);
-        console.log('afterInput.....'+this.value);
+        if(this.dataset.name=="number-people"){
+            myval=Number.parseInt(this.value);
+            if(regintegers.test(this.value) && myval==this.value){
+                this.value=this.value;
+            }
+            else if(this.value==""){
+                this.value="1";
+            }
+            else{
+                this.value=this.dataset.prevVale;
+            }
+            personsNumber=this.value;
+        }
+        else{
+            myval=Number.parseFloat(this.value);
+            if(regdecimals.test(this.value) && myval==this.value){
+                this.value=this.value;
+            }
+            else if(this.value==""){
+                this.value="0";
+            }
+            else{
+                this.value=this.dataset.prevVale;
+            } 
+            bill=this.value;
+          
+        }
+        document.querySelector("#tipAmoutValue").innerText=("$ "+(bill/100*tip).toFixed(2));
+        document.querySelector("#totalValue").innerText=("$ "+((bill/100*tip)/parseInt(personsNumber)).toFixed(2));
+
     })
 })
+
+document.querySelectorAll(".input-group ul li").forEach((element)=>{
+    element.addEventListener("click", function(){
+        this.parentNode.querySelector(".active").classList.remove("active");
+        this.classList.add("active");
+        if(this.querySelector("input").dataset.value){
+            tip=parseInt(this.querySelector("input").dataset.value);
+        }
+        else{
+             tip=parseInt(this.querySelector("input").value);
+        }
+        document.querySelector("#tipAmoutValue").innerText=("$ "+(bill/100*tip).toFixed(2));
+        document.querySelector("#totalValue").innerText=("$ "+((bill/100*tip)/parseInt(personsNumber)).toFixed(2));
+    });
+});
